d7fffcdaf9ace67241b84f61d39f9f055b6f49da
Nicolle Nunes <nicolle.nunes@nicolle.nunes>
2023-04-04 16:32:42 -0300

    feat: using fn as parameter
    Signed-off-by: Nicolle Nunes <nicolle.nunes@nicolle.nunes>

--- src/brave_and_true/descontos.clj
+++ src/brave_and_true/descontos.clj
@@ -18,12 +18,29 @@
   "DOC: Retorna o valor com desconto de 10% para valores > 100"
   [valor-bruto]
   ((deve-aplicar-desconto? valor-bruto)
-    (let [taxa-de-desconto (/ 10 100)
-          desconto         (* valor-bruto taxa-de-desconto)]
-      (println "Calculando desconto de" desconto)
-      (- valor-bruto desconto))
+   (let [taxa-de-desconto (/ 10 100)
+         desconto         (* valor-bruto taxa-de-desconto)]
+     (println "Calculando desconto de" desconto)
+     (- valor-bruto desconto))
 
-     valor-bruto))
+   valor-bruto))
 
 (valor-descontado-maior-100 100)
 (valor-descontado-maior-100 900)
+
+; Utilizando função como parâmetro
+
+(defn valor-descontado-condicional
+  "DOC: Retorna o valor com desconto caso o valor bruto atenda a uma condição"
+  [aplica? valor-bruto]
+  ((aplica? valor-bruto)
+   (let [taxa-de-desconto (/ 10 100)
+         desconto         (* valor-bruto taxa-de-desconto)]
+     (println "Calculando desconto de" desconto)
+     (- valor-bruto desconto))
+
+   valor-bruto))
+
+
+(valor-descontado-condicional deve-aplicar-desconto? 100)
+(valor-descontado-condicional deve-aplicar-desconto? 1000)
