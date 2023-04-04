81638f3a7300f1edd01ed276c650849a5350a189
Nicolle Nunes <nicolle.nunes@nicolle.nunes>
2023-04-04 14:04:14 -0300

    feat: deve aplicar?

--- src/brave_and_true/descontos.clj
+++ src/brave_and_true/descontos.clj
@@ -1,7 +1,7 @@
 (ns brave-and-true.descontos)
 
 ; Desconto de 10% para qualquer valor
-(defn valor-descontado
+(defn valor-descontado-todo-valor
   "DOC: Retorna o valor com desconto de 10%"
   [valor-bruto]
   (let [taxa-de-desconto (/ 10 100)
@@ -9,11 +9,15 @@
     (println "Calculando desconto de" desconto)
     (- valor-bruto desconto)))
 
+(defn deve-aplicar-desconto?
+  [valor-bruto]
+  (> valor-bruto 100))
+
 ; Desconto de 10% para valores > 100
-(defn valor-descontado2
+(defn valor-descontado-maior-100
   "DOC: Retorna o valor com desconto de 10% para valores > 100"
   [valor-bruto]
-  (if (> valor-bruto 100)
+  ((deve-aplicar-desconto? valor-bruto)
     (let [taxa-de-desconto (/ 10 100)
           desconto         (* valor-bruto taxa-de-desconto)]
       (println "Calculando desconto de" desconto)
@@ -21,5 +25,5 @@
 
      valor-bruto))
 
-(valor-descontado2 100)
-(valor-descontado2 900)
+(valor-descontado-maior-100 100)
+(valor-descontado-maior-100 900)
